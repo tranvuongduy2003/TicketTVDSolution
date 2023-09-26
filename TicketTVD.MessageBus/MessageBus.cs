2f5b0bdadb930d7c2692a823f4531da8771d3c57
Tran Vuong Duy <tranvuongduy2003@gmail.com>
2023-09-26 14:45:41 +0700

    init source

---
+++ TicketTVD.MessageBus/MessageBus.cs
@@ -0,0 +1,9 @@
+﻿namespace TicketTVD.MessageBus;
+
+public class MessageBus : IMessageBus
+{
+    public async Task PublishMessage(object message, string topic_queue_Name)
+    {
+        await new Task(null);
+    }
+}

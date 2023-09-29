9dc357f5285189debfafa91fa53a93de4762359f
Tran Vuong Duy <tranvuongduy2003@gmail.com>
2023-09-29 10:37:16 +0700

    Add DB Context and define ApplicationUser Model

---
+++ TicketTVD.Services.AuthAPI/Data/AppDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using TicketTVD.Services.AuthAPI.Models;
+
+namespace TicketTVD.Services.AuthAPI.Data;
+
+public class AppDbContext : IdentityDbContext<ApplicationUser>
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        
+        modelBuilder.Entity<ApplicationUser>()
+            .Property(e => e.CreatedAt)
+            .HasDefaultValueSql("getdate()");
+
+        modelBuilder.Entity<ApplicationUser>()
+            .Property(e => e.UpdatedAt)
+            .HasDefaultValueSql("getdate()");
+    }
+}
---
+++ TicketTVD.Services.AuthAPI/Models/ApplicationUser.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace TicketTVD.Services.AuthAPI.Models;
+
+public class ApplicationUser : IdentityUser
+{
+    public string Name { get; set; }
+    public DateTime DOD { get; set; }
+    public string Gender { get; set; }
+    public string Avatar { get; set; }
+    public string Status { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+}

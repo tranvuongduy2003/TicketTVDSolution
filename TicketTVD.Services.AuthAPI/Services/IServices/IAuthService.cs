05328d4290a56b59c996017a5be9484482e18774
Tran Vuong Duy <tranvuongduy2003@gmail.com>
2023-09-29 11:44:28 +0700

    Add auth service interface and its models

---
+++ TicketTVD.Services.AuthAPI/Services/IServices/IAuthService.cs
@@ -0,0 +1,10 @@
+﻿using TicketTVD.Services.AuthAPI.Models.Dto;
+
+namespace TicketTVD.Services.AuthAPI.Services.IServices;
+
+public interface IAuthService
+{
+    Task<string> Register(RegistrationRequestDto registrationRequestDto);
+    Task<LoginResponseDto> Login(LoginRequestDto loginRequestDto);
+    Task<bool> AssignRole(string email, string roleName);
+}

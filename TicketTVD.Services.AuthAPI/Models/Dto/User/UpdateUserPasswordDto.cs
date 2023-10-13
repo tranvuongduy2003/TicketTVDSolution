5a34c4db77f6cd14d48f287f0c7f13265705d2e0
Tran Vuong Duy <tranvuongduy2003@gmail.com>
2023-10-14 03:29:47 +0700

    Done UserAPI

---
+++ TicketTVD.Services.AuthAPI/Models/Dto/User/UpdateUserPasswordDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TicketTVD.Services.AuthAPI.Models.Dto;
+
+public class UpdateUserPasswordDto
+{
+    [Required]
+    public string OldPassword { get; set; }
+    [Required]
+    public string NewPassword { get; set; }
+}
---
+++ TicketTVD.Services.AuthAPI/Services/UserService.cs
@@ -0,0 +1,93 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.OpenApi.Extensions;
+using TicketTVD.Services.AuthAPI.Data;
+using TicketTVD.Services.AuthAPI.Models;
+using TicketTVD.Services.AuthAPI.Models.Dto;
+using TicketTVD.Services.AuthAPI.Services.IServices;
+
+namespace TicketTVD.Services.AuthAPI.Services;
+
+public class UserService : IUserService
+{
+    private readonly AppDbContext _db;
+    private readonly UserManager<ApplicationUser> _userManager;
+    private readonly RoleManager<IdentityRole> _roleManager;
+    private readonly IMapper _mapper;
+
+    public UserService(AppDbContext db, UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager, IMapper mapper)
+    {
+        _db = db;
+        _userManager = userManager;
+        _roleManager = roleManager;
+        _mapper = mapper;
+    }
+
+    public async Task<IEnumerable<UserDto>> GetUsers()
+    {
+        var users = _db.ApplicationUsers.ToList();
+        var userDtos = _mapper.Map<IEnumerable<UserDto>>(users);
+        return userDtos;
+    }
+
+    public async Task<UserDto?> GetUserById(string userId)
+    {
+        var user = _db.ApplicationUsers.FirstOrDefault(u => u.Id == userId);
+
+        if (user is null)
+        {
+            return null;
+        }
+
+        var userDto = _mapper.Map<UserDto>(user);
+        return userDto;
+    }
+
+    public async Task<UserDto?> UpdateUser(string userId, UpdateUserDto updateUserDto)
+    {
+        var user = _db.ApplicationUsers.FirstOrDefault(u => u.Id == userId);
+
+        if (user is null)
+        {
+            return null;
+        }
+
+        var updatedUser = _mapper.Map<ApplicationUser>(updateUserDto);
+        updatedUser.Id = userId;
+        _userManager.UpdateAsync(updatedUser);
+        
+        if (!_roleManager.RoleExistsAsync(updateUserDto.Role.GetDisplayName()).GetAwaiter()
+                .GetResult())
+        {
+            _roleManager.CreateAsync(new IdentityRole(updateUserDto.Role.GetDisplayName()))
+                .GetAwaiter().GetResult();
+        }
+
+        var userRoles = await _userManager.GetRolesAsync(updatedUser);
+        await _userManager.RemoveFromRolesAsync(updatedUser, userRoles);
+        await _userManager.AddToRoleAsync(updatedUser, updateUserDto.Role.GetDisplayName());
+
+        var updatedUserDto = _mapper.Map<UserDto>(updatedUser);
+        return updatedUserDto;
+    }
+
+    public async Task<string?> UpdateUserPassword(string userId, UpdateUserPasswordDto updateUserPasswordDto)
+    {
+        var user = _db.ApplicationUsers.FirstOrDefault(u => u.Id == userId);
+        if (user is null)
+        {
+            return null;
+        }
+
+        bool isValid = await _userManager.CheckPasswordAsync(user, updateUserPasswordDto.OldPassword);
+        if (!isValid)
+        {
+            return "Old password is wrong";
+        }
+
+        await _userManager.ChangePasswordAsync(user, updateUserPasswordDto.OldPassword,
+            updateUserPasswordDto.NewPassword);
+
+        return "";
+    }
+}
